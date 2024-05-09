0530b32b7c94ef504d9be173bc2949232f4fb427
cohenchris <40076937+cohenchris@users.noreply.github.com>
2024-05-09 12:02:13 -0700

    add qbittorrent connectivity testing script

---
+++ test_qbittorrent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source $(dirname "$0")/.env
+
+# Query vpn container to get public IP
+PUBLIC_IP=$(curl -s localhost:8001/v1/publicip/ip | jq -r ".public_ip")
+# Query vpn container to get forwarded port
+PORT=$(grep "VPN_PORTS" ${SERVER_DIR}/.env | cut -d '=' -f 2)
+
+nc -z $PUBLIC_IP $PORT
+
+# If not connectable, restart
+if [ $? -ne 0 ]; then
+  docker restart vpn
+  docker restart qbittorrent
+fi
