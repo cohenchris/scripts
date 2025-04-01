5b24f2b0743caefcc49f4c1ea09035eac6c6da0b
cohenchris <40076937+cohenchris@users.noreply.github.com>
2025-03-31 19:05:54 -0700

    nextcloud ffmpeg script upgrade packages after updating repos

--- system/server/nextcloud/nextcloud-install-ffmpeg.sh
+++ system/server/nextcloud/nextcloud-install-ffmpeg.sh
@@ -3,5 +3,6 @@
 if ! dpkg -s ffmpeg &> /dev/null; then
   # Install ffmpeg if not present
   docker exec nextcloud apt -y update
+  docker exec nextcloud apt -y upgrade
   docker exec nextcloud apt -y install libmagickcore-6.q16-6-extra ffmpeg
 fi
